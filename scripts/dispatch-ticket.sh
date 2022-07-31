036ea99caffdc76d6700d9df2b22869d50e2fbdb
Gui Seek <guiseek@gmail.com>
2022-07-31 15:04:06 -0300

    chore: build ssr

---
+++ scripts/dispatch-ticket.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# README: replace :token and :username
+curl -H "Authorization: token :token" \
+    -H 'Accept: application/vnd.github.everest-preview+json' \
+    "https://api.github.com/repos/DeveloperParana/recursos/dispatches" \
+    -d '{ "event_type": "prerender-ticket", "client_payload": { "username": ":username" }'
