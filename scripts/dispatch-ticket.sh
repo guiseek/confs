3cd6b9d00914dbd2eda50e0270866596672f5e03
Gui Seek <guiseek@gmail.com>
2022-07-31 16:03:52 -0300

    fix: base href

--- scripts/dispatch-ticket.sh
+++ scripts/dispatch-ticket.sh
@@ -3,5 +3,5 @@
 # README: replace :token and :username
 curl -H "Authorization: token :token" \
     -H 'Accept: application/vnd.github.everest-preview+json' \
-    "https://api.github.com/repos/DeveloperParana/recursos/dispatches" \
+    "https://api.github.com/repos/guiseek/confs/dispatches" \
     -d '{ "event_type": "prerender-ticket", "client_payload": { "username": ":username" }'
